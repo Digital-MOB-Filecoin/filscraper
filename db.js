0c44b1cf1270dc30243447d47e4bab696d034adf
stefangeorgerobert <stefan_george_robert@yahoo.com>
2023-02-01 00:33:50 +0200

    Update db.js

--- db.js
+++ db.js
@@ -998,14 +998,9 @@ class DB {
             await this.Query("REFRESH MATERIALIZED VIEW CONCURRENTLY fil_location_view WITH DATA;", 'RefreshEmissionsMatViews');
             await this.Query("REFRESH MATERIALIZED VIEW CONCURRENTLY fil_wt_view WITH DATA;", 'RefreshEmissionsMatViews');
             await this.Query("REFRESH MATERIALIZED VIEW CONCURRENTLY fil_un_view WITH DATA;", 'RefreshEmissionsMatViews');
-            //await this.Query("REFRESH MATERIALIZED VIEW CONCURRENTLY fil_emissions_view_v2 WITH DATA;", 'RefreshEmissionsMatViews');
-            //await this.Query("REFRESH MATERIALIZED VIEW CONCURRENTLY fil_miners_data_view WITH DATA;", 'RefreshEmissionsMatViews');
-            //await this.Query("REFRESH MATERIALIZED VIEW CONCURRENTLY fil_miners_data_view_country_v2 WITH DATA;", 'RefreshEmissionsMatViews');
-
-            //DISABLE
-            //await this.Query("REFRESH MATERIALIZED VIEW CONCURRENTLY fil_miner_view_days_lily_v1 WITH DATA;", 'RefreshEmissionsMatViews');
-            //await this.Query("REFRESH MATERIALIZED VIEW CONCURRENTLY fil_emissions_view_v3 WITH DATA;", 'RefreshEmissionsMatViews');
-            //await this.Query("REFRESH MATERIALIZED VIEW CONCURRENTLY fil_miners_data_view_country_v3 WITH DATA;", 'RefreshEmissionsMatViews');
+            await this.Query("REFRESH MATERIALIZED VIEW CONCURRENTLY fil_miner_view_days_lily_v1 WITH DATA;", 'RefreshEmissionsMatViews');
+            await this.Query("REFRESH MATERIALIZED VIEW CONCURRENTLY fil_emissions_view_v3 WITH DATA;", 'RefreshEmissionsMatViews');
+            await this.Query("REFRESH MATERIALIZED VIEW CONCURRENTLY fil_miners_data_view_country_v3 WITH DATA;", 'RefreshEmissionsMatViews');
         } catch (err) {
             WARNING(`[RefreshEmissionsMatViews] ${err}`)
         }
