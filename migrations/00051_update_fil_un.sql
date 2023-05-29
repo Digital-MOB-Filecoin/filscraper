4411c1dd57f3af44961f34b5e8a82435f4ddc3c6
stefangeorgerobert <stefan_george_robert@yahoo.com>
2023-05-29 23:59:44 +0300

    Update 00051_update_fil_un.sql

--- migrations/00051_update_fil_un.sql
+++ migrations/00051_update_fil_un.sql
@@ -1,2 +1,9 @@
-INSERT INTO fil_un (country, value) VALUES ('SN', 870);
+INSERT INTO fil_un (country, value)
+SELECT 'SN', 870
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM fil_un
+    WHERE country = 'SN'
+)
+
 UPDATE fil_un SET value = 68 WHERE country = 'SE';
