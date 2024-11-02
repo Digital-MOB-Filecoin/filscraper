c147e66fde177be00db3576a1e0c4ea288d47b36
zenstok <raressh_27@yahoo.com>
2024-11-02 22:08:54 +0200

    Stop creating unnecesary index (duplicated by primary key)

--- migrations/00071_create-fil-miners-confidence-scores.sql
+++ migrations/00071_create-fil-miners-confidence-scores.sql
@@ -4,5 +4,3 @@ CREATE TABLE IF NOT EXISTS fil_miners_confidence_scores (
     confidence_score NUMERIC NOT NULL,
     PRIMARY KEY (miner, date)
     );
-
-CREATE UNIQUE INDEX IF NOT EXISTS idx_miners_confidence_scores ON fil_miners_confidence_scores(miner, date);
